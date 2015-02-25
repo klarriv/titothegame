45d14040e18ba7b437280b234b39e9feee954872
Keven-Matthew <km@larriveefontaine.ca>
2015-02-25 15:15:12 -0500

    UML into code

---
+++ src/windows/MainFrame.java
@@ -0,0 +1,14 @@
+package windows;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame{
+	private LevelSelectMenu levelSelectMenu;
+	private MainMenu mainMenu;
+	private JPanel cardLayout;
+	
+	public MainFrame(){
+		cardLayout.setLayout(new CardLayout());
+	}
+}
