12891e916901dc794320b014203b377422869acc
Keven-Matthew <km@larriveefontaine.ca>
2015-02-25 15:31:48 -0500

    New run game

--- src/RunGame.java
+++ src/RunGame.java
@@ -2,32 +2,15 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-import windows.LevelSelectMenu;
-import windows.MainMenu;
-import windows.OptionMenu;
+import windows.*;
 
-
-/*
- * 
- */
-
-public class RunGame extends JFrame{
+public class RunGame{
 	LevelSelectMenu lvlSelectMenu = new LevelSelectMenu();
 	MainMenu mainMenu = new MainMenu();
 	OptionMenu optionMenu = new OptionMenu();
 	
-	RunGame(){
-		JFrame MainFrame = new JFrame();
-		MainFrame.add(mainMenu);
-		
-	}
-	
 	public static void main(String[] args){
-		RunGame game = new RunGame();
-		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		game.setResizable(false);
-		game.setSize(new Dimension (1280, 720));
-		game.setVisible(true);
+		MainFrame frame = new MainFrame();
 		
 	}
 }
