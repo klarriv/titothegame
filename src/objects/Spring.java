77c77e2430f81aefce1efa0d6dc8296bb587486b
Keven-Matthew <km@larriveefontaine.ca>
2015-04-01 14:45:14 -0400

    added automatic methods

--- src/objects/Spring.java
+++ src/objects/Spring.java
@@ -1,5 +1,6 @@
 package objects;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -17,6 +18,108 @@ public class Spring extends Physics{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public BufferedImage loadImage() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setWeight(int weight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setVisible(boolean isVisible) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isUsed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setUsed(boolean isUsed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DoublePoint getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPosition(DoublePoint position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setY(double y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setX(double x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getVx() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setVx() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setVx(double vx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getVy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setVy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setVy(double vy) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
